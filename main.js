42c77a6489a6060b19ac40c4328771b3d4e4829d
Owais <87132814+captain0owais@users.noreply.github.com>
2023-07-29 09:56:06 +0530

    Create main.js

---
+++ main.js
@@ -0,0 +1,13 @@
+
+    function error(){
+        console.error("checking...");
+    }
+
+    function hack(){
+        for( i=0;i<30;i+0.5 ){
+            error();
+          console.log(321 + 123);
+        }
+      console.log("thats how i like it");
+      console.error("SIUUUU");
+    }
